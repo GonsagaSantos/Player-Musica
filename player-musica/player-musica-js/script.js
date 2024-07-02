fd4b5ef8e87c5731db0690f4cdb860de83f13093
Miguel Santos <miguellsantoss915@gmail.com>
2024-07-02 03:18:20 -0300

    Adição de funcionalidades essenciais para um player de música, como botão de like, shuffle e repeat. Assim como a exibição da duração da música e tempo atual.

--- player-musica/player-musica-js/script.js
+++ player-musica/player-musica-js/script.js
@@ -1,5 +1,5 @@
-const songName = document.getElementById("song-name"); //o valor da const é imutável
-const bandName = document.getElementById("artist-name"); //getElementById é autoexplicativo, pega o elemento do HTML pela ID que está na tag
+const songName = document.getElementById("song-name");
+const bandName = document.getElementById("artist-name");
 const song = document.getElementById("audio");
 const cover = document.getElementById("cover");
 const play = document.getElementById("play");
@@ -8,63 +8,86 @@ const previous = document.getElementById("previous");
 const currentProgress = document.getElementById("current-progress");
 const progressContainer = document.getElementById("progress-container");
 const shuffle = document.getElementById("shuffle");
+const repeat = document.getElementById("repeat");
+const like = document.getElementById("like");
+const totalTime = document.getElementById("total-time");
+const playingTime = document.getElementById("playing-time");
 
 const Nirvana = {
     songName : "Come As You Are",
     artist : "Nirvana",
-    file : "nevermind"
+    file : "nevermind",
+    liked: false
 }
 
 const ArthurVerocai = {
     songName : "Dedicada a Ela",
     artist : "Arthur Verocai",
-    file : "arthurverocai"
+    file : "arthurverocai",
+    liked: false
 }
 
 const Deftones = {
     songName : "Changes",
     artist : "Deftones",
-    file : "whitepony"
+    file : "whitepony",
+    liked: false
 }
 
-let isPlaying = false; //a let é mutável
-let isShuffled = false;
-const originalPlaylist = [Nirvana, ArthurVerocai, Deftones]; //uma array
-let sortedPlaylist = [...originalPlaylist]; //as reticências são "spread"
+let isPlaying = false;
+let isShuffled = false; 
+const originalPlaylist = JSON.parse(localStorage.getItem('playlist')) ?? [Nirvana, ArthurVerocai, Deftones];
+let sortedPlaylist = [...originalPlaylist];
 let index = 0;
+let isFinish = false;
+let buttonRepeat_clicked = false;
 
-function playsong(){ //declarou uma função
-    play.querySelector(".bi").classList.remove("bi-play-circle-fill"); //querySelector pesquisou a classe .bi 
-    play.querySelector(".bi").classList.add("bi-pause-circle-fill"); //classlist.add procura um elemento dentro da classe, add e remove estão substituindo os iconss
+function playsong(){ 
+    play.querySelector(".bi").classList.remove("bi-play-circle-fill");
+    play.querySelector(".bi").classList.add("bi-pause-circle-fill");
     song.play();
-    isPlaying = true; //agora ela está como true, porque a função é da música tocando
+    isPlaying = true;
 }
 function pausesong(){
     play.querySelector(".bi").classList.remove("bi-pause-circle-fill");
     play.querySelector(".bi").classList.add("bi-play-circle-fill");
     song.pause();
-    isPlaying = false; //enquanto aqui ela está como falsa, porque está na função song.pause()
+    isPlaying = false;
 }
 
-function PlayDecider() { //declarei uma função que decide se pausa ou não, dependendo da situação das funções acima
-    if (isPlaying === true) { //if e else básico
+function PlayDecider() {
+    if (isPlaying) {
         pausesong();
     } else {
         playsong();
     }
 }
 
+ function likeButtonRender(){
+    if(sortedPlaylist[index].liked === false){ 
+        like.querySelector(".bi").classList.remove("bi-heart");
+        like.querySelector(".bi").classList.add("bi-heart-fill");
+        like.style.color = 'white';
+    } 
+    else {
+        like.querySelector(".bi").classList.remove("bi-heart-fill");
+        like.querySelector(".bi").classList.add("bi-heart");
+    }
+
+}
+
 function initializeSong(){
-    cover.src = `images/${originalPlaylist[index].file}.jpg`; //uma função que atualiza dinamicamente
-    song.src = `songs/${originalPlaylist[index].file}.mp3` //cada um dos elementos do site
-    songName.innerText = originalPlaylist[index].songName; //de acordo com a array que você citou
-    bandName .innerText = originalPlaylist[index].artist;
+    cover.src = `../../images/${sortedPlaylist[index].file}.jpg`;
+    song.src = `../../songs/${sortedPlaylist[index].file}.mp3`;
+    songName.innerText = sortedPlaylist[index].songName;
+    bandName.innerText = sortedPlaylist[index].artist;
+    likeButtonRender();
 }
 
 function previousSong() {
     if(index === 0){
-        index = originalPlaylist.length - 1;
-    }  else {
+        index = sortedPlaylist.length - 1;
+    } else {
         index -= 1;
     }
     initializeSong();
@@ -72,9 +95,9 @@ function previousSong() {
 }
 
 function nextSong() {
-    if(index === originalPlaylist.length - 1){
+    if(index === sortedPlaylist.length - 1){
         index = 0;
-    }  else {
+    } else {
         index += 1;
     }
     initializeSong();
@@ -82,22 +105,21 @@ function nextSong() {
 }
 
 function updateProgressBar(){
-    const barWidth = (song.currentTime/song.duration)*100;
-    currentProgress.style.setProperty("--progress", `${barWidth}%`)
-
+    const barWidth = (song.currentTime / song.duration) * 100;
+    currentProgress.style.setProperty("--progress", `${barWidth}%`);
 }
 
 function jumpTo(event){
     const width = progressContainer.clientWidth;
     const clickPosition = event.offsetX;
-    const jumpToTime = (clickPosition/width)*song.duration;
+    const jumpToTime = (clickPosition / width) * song.duration;
     song.currentTime = jumpToTime;
 }
 
 function shuffleArray(preShuffleArray) {
     const size = preShuffleArray.length;
     let currentIndex = size - 1;
-    while (currentIndex > 0) {
+    while (currentIndex >= 0) {
         let randomIndex = Math.floor(Math.random() * size);
         let aux = preShuffleArray[currentIndex];
         preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
@@ -110,20 +132,80 @@ function shuffleArray(preShuffleArray) {
 function shuffleClicked() {
     if (isShuffled === false) {
         isShuffled = true;
-        shuffleArray(sortedPlaylist);
-        shuffle.querySelector(".bi").classList.add("bi-shuffle");
+        sortedPlaylist = shuffleArray([...originalPlaylist]);
+        this.style.color = '#133955';
     } else {
         isShuffled = false;
         sortedPlaylist = [...originalPlaylist];
-        shuffle.querySelector(".bi").classList.remove("bi-shuffle");
+        this.style.color = 'white';
     }
+}
+
+function repeatButtonClicked() {
+    if(buttonRepeat_clicked === false){
+        buttonRepeat_clicked = true;  
+        this.style.color = '#133955';
+    }
+    else {
+        buttonRepeat_clicked = false;
+        this.style.color = 'white';
+    }
+}
+
+function nextOrRepeat() { 
+    if(buttonRepeat_clicked === false){
+        nextSong();
+    }
+    else {
+        playsong();
+    }
+}
+
+function timeFormated(originalNumber) {  
+    let hours = Math.floor(originalNumber / 3600);
+    let minutes = Math.floor((originalNumber - hours * 3600) / 60);
+    let seconds = Math.floor((originalNumber - hours * 3600 - minutes * 60));
+
+    if(originalNumber >= 3600){
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+    else {
+        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+}
+
+function updateCurrentTime() {
+    playingTime.innerText = timeFormated(song.currentTime);
+}
+
+function updateTotalTime() {
+    totalTime.innerText = timeFormated(song.duration);
+}
+
+function likeButtonClicked() {
+    if(sortedPlaylist[index].liked === true) {
+        sortedPlaylist[index].liked = false;
+    }
+    else {
+        sortedPlaylist[index].liked = true;
+    }
+    likeButtonRender();
+    localStorage.setItem(
+        'playlist',
+        JSON.stringify(originalPlaylist));
 }
 
 initializeSong();
 
-play.addEventListener("click", PlayDecider); //um evento de click, que chama e exerce a função playdecider
-previous.addEventListener("click", previousSong); //a mesma coisa com a respectiva função
+play.addEventListener("click", PlayDecider);
+previous.addEventListener("click", previousSong);
 next.addEventListener("click", nextSong);
 song.addEventListener("timeupdate", updateProgressBar);
+song.addEventListener("timeupdate", updateCurrentTime);
+song.addEventListener("ended", nextOrRepeat);
+song.addEventListener("loadedmetadata", updateTotalTime);
 progressContainer.addEventListener("click", jumpTo);
 shuffle.addEventListener("click", shuffleClicked);
+repeat.addEventListener("click", repeatButtonClicked);
+like.addEventListener("click", likeButtonClicked);
+playingTime.addEventListener("timeupdate", updateCurrentTime);
